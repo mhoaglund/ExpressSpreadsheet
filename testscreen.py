75697ee5ca05a92d967284ba5a1e12f18e70f357
mhoaglund <maxwell.hoaglund@gmail.com>
2019-10-14 19:05:55 -0500

    font stuff

--- testscreen.py
+++ testscreen.py
@@ -8,15 +8,16 @@ i2c = busio.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
 oled.fill(255)
 oled.show()
-font = ImageFont.load_default()
+font = ImageFont.truetype("OpenSans-Regular.ttf", 14, encoding = "unic")
 image = Image.new('1', (oled.width, oled.height))
 draw = ImageDraw.Draw(image)
 
 def updateScreen(message, pumpstate):
-    text = message
+    text = message + "   Pump is "
     (font_width, font_height) = font.getsize(text)
     draw.text((oled.width//2 - font_width//2, oled.height//2 - font_height//2),
     text, font=font, fill=255)
+    oled.image(image)
     oled.show()
 
 updateScreen("Hello World", "On")
